cf5ed45709c40ad0909d1c2c3885a49451d54f51
Es7 <115493775+EsdrasSouza7@users.noreply.github.com>
2022-12-08 09:10:05 -0300

    Delete passageiro.py

--- passageiro.py
+++
@@ -1,7 +0,0 @@
-class Passageiros:
-
-    def __init__(self, nome, data_de_ida, local_destino, telefone):
-        self.Nome = nome
-        self.telefone = telefone
-        self.data = data_de_ida
-        self.local = local_destino
